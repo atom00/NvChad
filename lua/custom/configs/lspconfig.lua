a2a418942212ed7659becbd0a4d46479761d9bce
Tomasz Bohutyn <tomasz@bohutyn.dev>
2024-04-19 22:37:57 +0200

    surround and autoformat

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -1,4 +1,5 @@
 local base = require("plugins.configs.lspconfig")
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 local on_attach = base.on_attach
 local capabilities = base.capabilities
 
@@ -6,6 +7,21 @@ local lspconfig = require("lspconfig")
 
 lspconfig.clangd.setup {
   on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({
+        group = augroup,
+        buffer = bufnr,
+      })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({
+            bufnr = bufnr,
+          })
+        end,
+      })
+    end
     client.server_capabilities.signatureHelpProvider = false
     on_attach(client, bufnr)
   end,
@@ -14,6 +30,21 @@ lspconfig.clangd.setup {
 
 lspconfig.rust_analyzer.setup{
   on_attach = function (client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({
+        group = augroup,
+        buffer = bufnr,
+      })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({
+            bufnr = bufnr,
+          })
+        end,
+      })
+    end
     client.server_capabilities.signatureHelpProvider = false
     on_attach(client, bufnr)
   end,
--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -19,6 +19,16 @@ local plugins = {
     end
   },
   {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+  },
+  {
     "nvim-neotest/nvim-nio"
   },
   {
